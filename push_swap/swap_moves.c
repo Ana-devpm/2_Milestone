b4fa95373b28c2eed6aa1ed8b85ec410835b87c2
Ana <ana.devpm@gmail.com>
2025-01-30 09:48:12 +0100

    feat(swap_moves.c: Norminette correction)

--- push_swap/swap_moves.c
+++ push_swap/swap_moves.c
@@ -6,7 +6,7 @@
 /*   By: afailde- <afailde-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 18:44:12 by afailde-          #+#    #+#             */
-/*   Updated: 2025/01/24 19:40:42 by afailde-         ###   ########.fr       */
+/*   Updated: 2025/01/30 09:47:33 by afailde-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,15 @@
 
 void	swap_a(t_stack *stack_a)
 {
-	t_node 	*fst_node;
+	t_node	*fst_node;
 	t_node	*sec_node;
 
-	if(!stack_a || !stack_a->node || !stack_a->node->next)
+	if (!stack_a || !stack_a->node || !stack_a->node->next)
 		return (1);
-	
 	fst_node = stack_a->node;
 	sec_node = fst_node->next;
-
 	fst_node->next = sec_node->next;
 	sec_node->next = fst_node;
-
 	stack_a->node = sec_node;
 }
 
@@ -36,14 +33,11 @@ void	swap_b(t_stack *stack_b)
 
 	if (!stack_b || !stack_b->node || !stack_b->node->next)
 		return (1);
-
 	fst_node = stack_b->node;
-    sec_node = fst_node->next;
-
-    fst_node->next = sec_node->next;
-    sec_node->next = fst_node;
-
-    stack_b->node = sec_node;
+	sec_node = fst_node->next;
+	fst_node->next = sec_node->next;
+	sec_node->next = fst_node;
+	stack_b->node = sec_node;
 }
 
 void	doble_swap(t_stacks *stacks)
